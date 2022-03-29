3275783e27e7fac41c3a8ac6c960ea4064691160
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-03-28 23:57:09 -0400

    fixing routes

--- Main/models/index.js
+++ Main/models/index.js
@@ -2,28 +2,20 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-User.hasMany(Post, {
-  foreignKey: 'user_id',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id'
-});
-
-// Comment.belongsToMany(Post,{
-//   foreignKey: 'comment_id'
-// })
 
 Comment.belongsTo(User,{
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 })
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 })
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id'
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
 })
 
 
